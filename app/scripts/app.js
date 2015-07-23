6ca79d8eff68eec6e75c1f4132bc5ed558866cab
Sebastian Kloppe <feedbackcalcest@gmail.com>
2015-07-23 16:27:13 +0200

    app basic structure

---
+++ app/scripts/app.js
@@ -0,0 +1,36 @@
+(function(document) {
+  'use strict';
+
+  var docReady = false;
+
+  // Grab a reference to our auto-binding template
+  // and give it some initial binding values
+  // Learn more about auto-binding templates at http://goo.gl/Dx1u2g
+  var app = document.querySelector('#app');
+
+  app.displayInstalledToast = function() {
+    document.querySelector('#caching-complete').show();
+  };
+
+  // Listen for template bound event to know when bindings
+  // have resolved and content has been stamped to the page
+  app.addEventListener('dom-change', function() {
+    console.log('Socobo Frontend app is ready to rock!');
+  });
+
+  // See https://github.com/Polymer/polymer/issues/1381
+  window.addEventListener('WebComponentsReady', function() {
+    // imports are loaded and elements have been registered
+    docReady = true;
+  });
+
+  // Close drawer after menu item is selected if drawerPanel is narrow
+  app.onMenuSelect = function() {
+    var drawerPanel = document.querySelector('#paperDrawerPanel');
+    if (docReady) {
+      if (drawerPanel.narrow) {
+        drawerPanel.closeDrawer();
+      }
+    }
+  };
+})(document);
