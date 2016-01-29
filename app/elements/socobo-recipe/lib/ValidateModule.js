4d5bccb1dbb8d69d4bf9ec11249c1c1ff3dbe829
hetec <hebner.tec@gmail.com>
2016-01-29 18:14:35 +0100

    ba-124 Refactoring

--- app/elements/socobo-recipe/lib/ValidateModule.js
+++ app/elements/socobo-recipe/lib/ValidateModule.js
@@ -1,76 +1,47 @@
-var ValidateModule = ValidateModule || (function(){
+var ValidateModule = ValidateModule || (function () {
 
-    var msgPrefix = "", errorMessages = [];
+    'use strict';
 
-    function _setMessagePrefix(prefix){
-      msgPrefix = prefix;
+    function _verifySinglePropertyValueNotEmpty(obj, property) {
+      return obj[property] !== "" && obj[property] !== undefined && obj[property] !== null;
     }
 
-    function _verifySinglePropertyValueNotEmpty(obj, property){
-      return obj[property] !== "" && obj[property] !== undefined && obj[property] != null;
-    }
-
-    function _verifyArrayNotEmpty(arr){
+    function _verifyArrayNotEmpty(arr) {
       return arr.length > 0;
     }
 
-    function _addSingleEntryToErrorMsg(snippet){
-      errorMessages.push(snippet);
-    }
-
-    function _addMultipleEntriesToErrorMsg(context, snippets){
-      errorMessages.push({
-        context : context,
-        snippets :  snippets
-      });
-    }
-
-    function _buildErrorMessage(messages){
-      var msg;
-      if (_verifyArrayNotEmpty(messages)) {
-        msg = msgPrefix;
-        messages.forEach(function(messagePart){
-          if(_isObject(messagePart)){
-            msg += messagePart.context + "("
-            if(_verifyArrayNotEmpty(messagePart.snippets)){
-              var subMsg = ""
-              messagePart.snippets.forEach(function(snippet){
-                subMsg += snippet + ", "
-              });
-              subMsg = subMsg.slice(0, subMsg.length - 2);
-              msg += subMsg + "), "
-            }
-          }else{
-            msg += element + ", ";
-          }
-          });
-        msg = msg.slice(0, msg.length - 2);
+    function _verifyAllPropertyValuesNotEmpty(obj, propertiesArray) {
+      var invalidProperties = [], prop, valid;
+      for (prop in propertiesArray) {
+        valid = _verifySinglePropertyValueNotEmpty(obj, propertiesArray[prop]);
+        if (!valid) {
+          invalidProperties.push(propertiesArray[prop]);
+        }
       }
-    }
-
-    /**
-     * Checks if a given parameter is an object
-     * @param o
-     * @returns {boolean}
-     * @private
-     */
-    function _isObject(o) {
-      return o !== null && typeof o === "object";
-    }
-
-    function _verifyAllPropertyValuesNotEmpty(arrLikeObj, propertiesArray){
-      var invalidProperties = [];
-      Array.prototype.forEach.call(arrLikeObj, function(currentOjb){
-        Array.prototype.forEach.call(propertiesArray, function(currentProperty){
-          var valid = _verifySinglePropertyValueNotEmpty(currentOjb, currentProperty);
-          if(!valid){
-            invalidProperties.push(currentProperty);
-          }
-        });
-      });
       return invalidProperties;
     }
 
+    function _verifyAllPropertyValuesOfAllObjectsNotEmpty(objArray, propertiesArray) {
+      var invalidProperties = [], objProp, tmp;
 
+      function elementInList(element) {
+        if (invalidProperties.indexOf(element) < 0) {
+          invalidProperties.push(element);
+        }
+      }
 
-  })();
+      for (objProp in objArray) {
+        tmp = _verifyAllPropertyValuesNotEmpty(objArray[objProp], propertiesArray);
+        tmp.forEach(elementInList);
+      }
+      return invalidProperties;
+    }
+
+    return {
+      verifySingleProperty : _verifySinglePropertyValueNotEmpty,
+      verifyMultipleProperties : _verifyAllPropertyValuesNotEmpty,
+      verifyAll : _verifyAllPropertyValuesOfAllObjectsNotEmpty,
+      verifyArrayNotEmpty : _verifyArrayNotEmpty
+    };
+
+  }());
