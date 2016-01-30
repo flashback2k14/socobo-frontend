6baa435d491c8c9ae6cbd2cee60d587fa4821178
hetec <hebner.tec@gmail.com>
2016-01-30 21:28:59 +0100

    ba-124 Docu and Lint fixes

--- app/elements/socobo-recipe/lib/MessageModule.js
+++ app/elements/socobo-recipe/lib/MessageModule.js
@@ -1,102 +1,31 @@
-var MessageModule = (function (utils) {
+var UtilsModule = UtilsModule || (function() {
 
-  'use strict';
+  "use strict";
 
-  var msgPrefix, errorMessages, wordMap;
-
-  function MessageModule() {
-    msgPrefix = "";
-    errorMessages = [];
-  }
-
-  function _setMessagePrefix(prefix){
-    msgPrefix = prefix;
-  }
-
-  function _setMappings(mappings) {
-    wordMap = mappings;
-  }
-
-  function _applyWordMap(messageString) {
-    var word, tmpMsg = messageString, regex;
-    console.log("Map", wordMap);
-    if (wordMap) {
-      for (word in wordMap) {
-        regex = new RegExp(word, "g");
-        tmpMsg = tmpMsg.replace(regex, wordMap[word]);
-      }
-    }
-    return tmpMsg;
-  }
-
-  function _addSingleEntryToErrorMsg(snippet){
-    errorMessages.push(snippet);
-  }
-
-  function _addMultipleEntriesToErrorMsg(context, snippets){
-    errorMessages.push({
-      context : context,
-      snippets :  snippets
-
-    });
-  }
-
-  function _buildErrorMessage() {
-    var msg = "";
-    if (utils.arrayNotEmpty(errorMessages)) {
-      msg = msgPrefix;
-      errorMessages.forEach(function(messagePart){
-        if (utils.isObject(messagePart)){
-          msg += messagePart.context + "(";
-          if (utils.arrayNotEmpty(messagePart.snippets)){
-            var subMsg = "";
-            messagePart.snippets.forEach(function(snippet){
-              subMsg += snippet + ", "
-            });
-            subMsg = subMsg.slice(0, subMsg.length - 2);
-            msg += subMsg + "), "
-          }
-        } else {
-          msg += messagePart + ", ";
-        }
-      });
-      msg = msg.slice(0, msg.length - 2);
-    }
-    return _applyWordMap(msg);
-  }
-
-  MessageModule.prototype.addMsgPart = _addSingleEntryToErrorMsg;
-  MessageModule.prototype.addMsgParts = _addMultipleEntriesToErrorMsg;
-  MessageModule.prototype.buildMsg = _buildErrorMessage;
-  MessageModule.prototype.setPrefix = _setMessagePrefix;
-  MessageModule.prototype.setMappings = _setMappings;
-
-  return MessageModule;
-
-}(UtilsModule));
-
-var UtilsModule = UtilsModule || (function () {
-
-  'use strict';
-
+  /**
+   * Verifies that an given array has a length > 0
+   *
+   * @param {Array} arr
+   * @returns {boolean}
+   * @private
+   */
   function _verifyArrayNotEmpty(arr) {
     return arr.length > 0;
   }
-
   /**
    * Checks if a given parameter is an object
-   * @param o
+   *
+   * @param {object} o
    * @returns {boolean}
    * @private
    */
   function _isObject(o) {
-
     return o !== null && typeof o === "object";
   }
-
   /**
    * Checks if a given parameter is an array
-   * @param o
+   *
+   * @param {object} o
    * @returns {boolean}
    * @private
    */
@@ -111,3 +40,134 @@ var UtilsModule = UtilsModule || (function () {
   };
 }());
 
+/**
+ * Enables building a message based on particular pieces
+ * - add single message pieces
+ * - add arrays of message pieces
+ * - provide mappings to translate specific pieces of the message
+ *
+ * Dependency: UtilsModule
+ */
+var MessageModule = (function(utils) {
+
+  "use strict";
+
+  var msgPrefix;
+  var errorMessages;
+  var wordMap;
+
+  /**
+   *
+   * @constructor
+   */
+  function MessageModule() {
+    msgPrefix = "";
+    errorMessages = [];
+  }
+  /**
+   *
+   * Set a message prefix which will be placed in from of
+   * the main message
+   *
+   * @param {String} prefix
+   * @private
+   */
+  function _setMessagePrefix(prefix) {
+    msgPrefix = prefix;
+  }
+  /**
+   *
+   * Provide a object with particular mappings.
+   * mappings = {original: newValue, ...}
+   * These phrases will be replace in the resulting message
+   *
+   * @param {Object} mappings
+   * @private
+   */
+  function _setMappings(mappings) {
+    wordMap = mappings;
+  }
+  /**
+   *
+   * Applies the provided mappings on a given message
+   *
+   * @param {String} messageString
+   * @returns {String}
+   * @private
+   */
+  function _applyWordMap(messageString) {
+    var word;
+    var tmpMsg = messageString;
+    var regex;
+    console.log("Map", wordMap);
+    if (wordMap) {
+      for (word in wordMap) {
+        regex = new RegExp(word, "g");
+        tmpMsg = tmpMsg.replace(regex, wordMap[word]);
+      }
+    }
+    return tmpMsg;
+  }
+  /**
+   * Add a single entry to the message
+   *
+   * @param {String} snippet
+   * @private
+     */
+  function _addSingleEntryToErrorMsg(snippet) {
+    errorMessages.push(snippet);
+  }
+  /**
+   * Add multiple entries to the message. The specified context
+   * describes where the entries belong to
+   *
+   * @param {String} context
+   * @param {Array} snippets
+   * @private
+   */
+  function _addMultipleEntriesToErrorMsg(context, snippets) {
+    errorMessages.push({
+      context: context,
+      snippets:  snippets
+    });
+  }
+  /**
+   * Builds the message based on the added pieces
+   *
+   * @returns {String}
+   * @private
+   */
+  function _buildErrorMessage() {
+    var msg = "";
+    if (utils.arrayNotEmpty(errorMessages)) {
+      msg = msgPrefix;
+      errorMessages.forEach(function(messagePart) {
+        if (utils.isObject(messagePart)) {
+          msg += messagePart.context + "(";
+          if (utils.arrayNotEmpty(messagePart.snippets)) {
+            var subMsg = "";
+            messagePart.snippets.forEach(function(snippet) {
+              subMsg += snippet + ", ";
+            });
+            subMsg = subMsg.slice(0, subMsg.length - 2);
+            msg += subMsg + "), ";
+          }
+        } else {
+          msg += messagePart + ", ";
+        }
+      });
+      msg = msg.slice(0, msg.length - 2);
+    }
+    return _applyWordMap(msg);
+  }
+
+  //Define the public interface
+  MessageModule.prototype.addMsgPart = _addSingleEntryToErrorMsg;
+  MessageModule.prototype.addMsgParts = _addMultipleEntriesToErrorMsg;
+  MessageModule.prototype.buildMsg = _buildErrorMessage;
+  MessageModule.prototype.setPrefix = _setMessagePrefix;
+  MessageModule.prototype.setMappings = _setMappings;
+
+  return MessageModule;
+
+}(UtilsModule));
--- app/elements/socobo-recipe/lib/ValidateModule.js
+++ app/elements/socobo-recipe/lib/ValidateModule.js
@@ -1,70 +1,139 @@
-var ValidateModule = ValidateModule || (function () {
+/**
+ * Provides different methods to validate data
+ *
+ * @type {{verifySinglePropertyNotEmpty, verifySingleProperty, verifyMultipleProperties,
+ * verifyAll, verifyArrayNotEmpty, isNumber, isString, isBoolean}}
+ */
+var ValidateModule = ValidateModule || (function() {
 
-    'use strict';
-
-    function _verifySinglePropertyValueNotEmpty(obj, property) {
-      return (!obj[property]) ? false : true;
+  "use strict";
+  /**
+   * Verifies that a single property is not empty
+   *
+   * @param {object} obj
+   * @param {*} property
+   * @returns {boolean}
+   * @private
+   */
+  function _verifySinglePropertyValueNotEmpty(obj, property) {
+    return (!obj[property]) ? false : true;
+  }
+  /**
+   * Verifies that a single property is not empty
+   * and that it is of the defined type
+   *
+   * @param {Object} obj
+   * @param {*} property
+   * @param {String} type
+   * @returns {boolean}
+   * @private
+   */
+  function _verifySingleProperty(obj, property, type) {
+    var valid;
+    valid = _verifySinglePropertyValueNotEmpty(obj, property);
+    if (type && valid) {
+      valid = (typeof obj[property]) === type;
     }
-
-    function _verifySingleProperty(obj, property, type){
-      var valid;
-      valid = _verifySinglePropertyValueNotEmpty(obj, property);
-      if (type && valid) {
-        valid = (typeof obj[property]) === type;
+    return valid;
+  }
+  /**
+   * Verifies all properties of the given object are not empty
+   * and of the given type. The types of the properties are specified
+   * in the properties object like var props = [{value: name, type: string}, ...]
+   * The properties are defined over the assigned array
+   *
+   * @param {Object} obj
+   * @param {Array} propertiesArray
+   * @returns {Array}
+   * @private
+   */
+  function _verifyAllPropertyValuesNotEmpty(obj, propertiesArray) {
+    var invalidProperties = [];
+    var prop;
+    var valid;
+    for (prop in propertiesArray) {
+      valid = _verifySingleProperty(obj, propertiesArray[prop].value, propertiesArray[prop].type);
+      if (!valid) {
+        invalidProperties.push(propertiesArray[prop].value);
       }
-      return valid;
     }
+    return invalidProperties;
+  }
+  /**
+   * Verifies all properties of all objects
+   *
+   * @param {Array} objArray
+   * @param {Array} propertiesArray
+   * @returns {Array}
+   * @private
+   */
+  function _verifyAllPropertyValuesOfAllObjectsNotEmpty(objArray, propertiesArray) {
+    var invalidProperties = [];
+    var objProp;
+    var tmp;
 
-    function _verifyAllPropertyValuesNotEmpty(obj, propertiesArray) {
-      var invalidProperties = [], prop, valid;
-      for (prop in propertiesArray) {
-        valid = _verifySingleProperty(obj, propertiesArray[prop].value, propertiesArray[prop].type);
-        if (!valid) {
-          invalidProperties.push(propertiesArray[prop].value);
-        }
+    function elementInList(element) {
+      if (invalidProperties.indexOf(element) < 0) {
+        invalidProperties.push(element);
       }
-      return invalidProperties;
     }
 
-    function _verifyAllPropertyValuesOfAllObjectsNotEmpty(objArray, propertiesArray) {
-      var invalidProperties = [], objProp, tmp;
-
-      function elementInList(element) {
-        if (invalidProperties.indexOf(element) < 0) {
-          invalidProperties.push(element);
-        }
-      }
-
-      for (objProp in objArray) {
-        tmp = _verifyAllPropertyValuesNotEmpty(objArray[objProp], propertiesArray);
-        tmp.forEach(elementInList);
-      }
-      return invalidProperties;
+    for (objProp in objArray) {
+      tmp = _verifyAllPropertyValuesNotEmpty(objArray[objProp], propertiesArray);
+      tmp.forEach(elementInList);
     }
+    return invalidProperties;
+  }
+  /**
+   * Verifies that array.length > 0
+   *
+   * @param {Array} arr
+   * @returns {boolean}
+   * @private
+   */
+  function _verifyArrayNotEmpty(arr) {
+    return arr.length > 0;
+  }
+  /**
+   * Verifies that the input value is of type number
+   *
+   * @param {*} input
+   * @returns {boolean}
+   * @private
+   */
+  function _isNumber(input) {
+    return typeof parseInt(input) === "number";
+  }
+  /**
+   * Verifies that the input value is of type string
+   *
+   * @param {*} input
+   * @returns {boolean}
+   * @private
+   */
+  function _isString(input) {
+    return typeof input === "string";
+  }
+  /**
+   * Verifies that the input value is of type boolean
+   *
+   * @param {*} input
+   * @returns {boolean}
+   * @private
+   */
+  function _isBoolean(input) {
+    return typeof input === "boolean";
+  }
 
-    function _verifyArrayNotEmpty(arr) {
-      return arr.length > 0;
-    }
-    function _isNumber(input) {
-      return typeof parseInt(input) === "number";
-    }
-    function _isString(input) {
-      return typeof input === "string";
-    }
-    function _isBoolean(input) {
-      return typeof input === "boolean";
-    }
+  return {
+    verifySinglePropertyNotEmpty: _verifySinglePropertyValueNotEmpty,
+    verifySingleProperty: _verifySingleProperty,
+    verifyMultipleProperties: _verifyAllPropertyValuesNotEmpty,
+    verifyAll: _verifyAllPropertyValuesOfAllObjectsNotEmpty,
+    verifyArrayNotEmpty: _verifyArrayNotEmpty,
+    isNumber: _isNumber,
+    isString: _isString,
+    isBoolean: _isBoolean
+  };
 
-    return {
-      verifySinglePropertyNotEmpty: _verifySinglePropertyValueNotEmpty,
-      verifySingleProperty: _verifySingleProperty,
-      verifyMultipleProperties: _verifyAllPropertyValuesNotEmpty,
-      verifyAll: _verifyAllPropertyValuesOfAllObjectsNotEmpty,
-      verifyArrayNotEmpty : _verifyArrayNotEmpty,
-      isNumber: _isNumber,
-      isString: _isString,
-      isBoolean: _isBoolean
-
-    };
-
-  }());
+}());
