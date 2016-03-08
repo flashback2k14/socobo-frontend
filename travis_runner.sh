5e844d02ce9da83c2129246b22bd5bc378d22691
Thomas Fiedler <thfied@gmail.com>
2016-03-08 12:36:07 +0100

    ba-59 Data ingegrity and constraint validation
    - Remove clutter from `travis_runner.sh`
    - set variable for g++
    - include $TRAVIS_COMMIT into the firebase deploy message

--- travis_runner.sh
+++ travis_runner.sh
@@ -3,24 +3,18 @@ set -o pipefail
 
 if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ]  && [ "$TRAVIS_NODE_VERSION" = "5.7.1" ]
 then
-  git config --global user.email "samccone@gmail.com"
-  git config --global user.name "auto deployer"
-
   deploy_firebase () {
-    # Deploying to Firebase! (https://polymer-starter-kit.firebaseapp.com)
+    # Deploying to Firebase!
     echo Deploying to Firebase
     # Starting Build Process for Firebase Changes
     firebase-bolt db/rules.bolt
     gulp
     # Starting Deploy Process to Firebaseapp.com Server -- polymer-starter-kit.firebaseapp.com
-    firebase deploy --token "$FIREBASE_TOKEN" -m "Auto Deployed by Travis CI"
+    firebase deploy --token "$FIREBASE_TOKEN" -m "Auto Deployed by Travis CI - $TRAVIS_COMMIT"
   }
 
   deploy_firebase
 
-  # Revert to orginal index.html and delete temp file
-  cp app/index.html.tmp1 app/index.html
-  rm app/index.html.tmp1
 elif [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ]  && [ "$TRAVIS_NODE_VERSION" != "5.7.1" ]
 then
   echo "Do Nothing, only deploy with Node 5.1"
